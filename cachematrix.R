6c066d7bd052ce5b2bb12f754bced5ea86c099d7
wurmologin <saskia.brauer@gmx.net>
2015-06-20 12:13:07 +0200

    changes for Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following function makeCacheMatrix creates a list containing:
+## 1. sets empty matix
+## 2. gets value of matix
+## 3. sets value of matix-inverse
+## 4. gets value of matix-inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+    ## initialize matrix inverse to NULL
+    mat_inverse <- NULL                     
+    ## make another function where the value will be cached in 1. Matrix is created
+    ## for the first time. 2. changes made to cached matrix
+    set <- function(y) {                      
+        x <<- y
+        ## change the value of inverse of the matrix in case the matrix was changed.
+        mat_inverse <<- NULL              
+    }
+    ## gets the value of the inverse
+    get <- function() x                           
+    #calculates the inverse 
+    setinverse <- function(solve) mat_inverse <<- solve 
+    # gets the inverse     
+    getinverse <- function() mat_inverse        
+    ## passes the value of the function makeCacheMatrix        
+    list(set = set, get = get,                    
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# used to get the cache of the matrix
+cacheSolve<- function(x, ...) {                 
+    mat_inverse <- x$getinverse()
+    #if the inverse exists, it gets it.
+    if(!is.null(mat_inverse)) {                 
+        message("getting cached data - Inverse of the matrix")
+        return(mat_inverse)
+    }
+    #if the inverse if not there, first it is calculated and then retrieved.
+    data <- x$get()                               
+    mat_inverse <- solve(data, ...)
+    x$setinverse(mat_inverse)
+    mat_inverse
 }
